88c4621e4274fbf4789788c22478d761beed27bc
Muhammad Shayan Jani <101168909+ShayanJani01@users.noreply.github.com>
2022-07-20 05:02:06 -0700

    Update index.php

--- connections/index.php
+++ connections/index.php
@@ -1,10 +1,21 @@
 <?php 
 
-//create a connection
-$conn= mysqli_connect("localhost","root","","connection");
-//check connection
+$db_host = "localhost";
+$db_user = "root";
+$db_password = "";
+$db_name = "connection";
+
+//created a connection
+
+$conn = mysqli_connect($db_host, $db_user, $db_password, $db_name);
+
+//checking the connection
+
 if($conn){
-echo"connection successfull";
+    echo "connection successfull";
+}
+else{
+    die("connection failed");
 }
 
 
